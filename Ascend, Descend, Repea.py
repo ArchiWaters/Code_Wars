939aedc331f2980eef6d12d41cbe9b0fac82f87a
Archi_Waters <engineer_waters@yahoo.com>
2022-07-23 19:09:36 +0300

    Initial commit

---
+++ Ascend, Descend, Repea.py
@@ -0,0 +1,8 @@
+def ascend_descend(length, minimum, maximum):
+    a = ''
+    for n in range(minimum, maximum):
+        return n
+
+ascend_descend(10,1,4)
+
+
---
+++ SevenAte9.py
@@ -0,0 +1,7 @@
+def seven_ate9(str_):
+    import re
+    str_2 = re.sub('9', '', str_)
+    return str_2
+
+
+seven_ate9('79797')
