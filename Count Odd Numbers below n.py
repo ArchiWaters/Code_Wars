ecf8c343fa08f917bcf3562b7a0a4bde37ee5bac
Archi_Waters <engineer_waters@yahoo.com>
2022-07-24 13:45:39 +0300

    change file Count Odd Numbers below n

--- Count Odd Numbers below n.py
+++ Count Odd Numbers below n.py
@@ -3,5 +3,7 @@ def odd_count(n):
     for s in range(n):
         if s % 2 == 1:
             odd.append(s)
+    print(odd)
     return odd
-#вернуть список нечетных числ меньше n
+# вернуть список нечетных числ меньше n
+odd_count(15)
