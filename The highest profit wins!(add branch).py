ab0f6a5e123afa31984847ece658c2a1c538952e
ArchiWaters <95975471+ArchiWaters@users.noreply.github.com>
2022-07-28 14:30:52 +0300

    Update The highest profit wins!(add branch).py

--- The highest profit wins!(add branch).py
+++ The highest profit wins!(add branch).py
@@ -1,8 +1,2 @@
 def min_max(lst):
-    my_lst = []
-    min_lst = min(lst)
-    my_lst.append(min_lst)
-    max_lst = max(lst)
-    my_lst.append(max_lst)
-    my_lst.sort()
-    return my_lst
+    return [min(lst), max(lst)]
